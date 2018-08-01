26d99e01fc580d620ef3b2ba414e25a812010cc1
Anders Blehr <anders@andersblehr.co>
2018-08-01 06:42:14 +0200

    Clean up layout and JS

--- assets/main.js
+++ assets/main.js
@@ -1,39 +1,27 @@
-var expandBelow = function(event) {
-    var expandBelowLink = event.target;
-    var expandableDetailSection = expandBelowLink.parentNode.nextElementSibling;
-    var collapseAboveLink = expandableDetailSection.nextElementSibling.firstChild;
-    expandBelowLink.classList.add('hidden');
-    expandableDetailSection.classList.remove('hidden');
-    collapseAboveLink.classList.remove('hidden');
+var expand = function(event) {
+    var expander = event.target;
+    var collapser = expander.nextElementSibling;
+    var expandable = expander.parentNode.nextElementSibling;
+    expander.classList.add('hidden');
+    expandable.classList.remove('hidden');
+    collapser.classList.remove('hidden');
 };
 
-var collapseAbove = function(event) {
-    var collapseAboveLink = event.target;
-    var expandableDetailSection = collapseAboveLink.parentNode.previousElementSibling;
-    var expandBelowLink = expandableDetailSection.previousElementSibling.firstChild;
-    collapseAboveLink.classList.add('hidden');
-    expandableDetailSection.classList.add('hidden');
-    expandBelowLink.classList.remove('hidden');
-
-    var precedingSibling = expandBelowLink.parentNode.previousElementSibling;
-    var didScroll = false;
-    while (!didScroll) {
-        if (precedingSibling.nodeName.substring(0, 1) == 'H') {
-            var scrollTarget = document.getElementById(precedingSibling.id).offsetTop;
-            window.scrollTo(0, scrollTarget);
-            didScroll = true;
-        } else {
-            precedingSibling = precedingSibling.previousElementSibling;
-        }
-    }
+var collapse = function(event) {
+    var collapser = event.target;
+    var expander = collapser.previousElementSibling;
+    var collapsable = collapser.parentNode.nextElementSibling;
+    collapser.classList.add('hidden');
+    collapsable.classList.add('hidden');
+    expander.classList.remove('hidden');
 };
 
-var expandBelowLinks = document.getElementsByClassName('expand-below');
-for (var i = 0; i < expandBelowLinks.length; i++) {
-    expandBelowLinks[i].addEventListener('click', expandBelow, false);
+var expanders = document.getElementsByClassName('expander');
+for (var i = 0; i < expanders.length; i++) {
+    expanders[i].addEventListener('click', expand, false);
 }
 
-var collapseAboveLinks = document.getElementsByClassName('collapse-above');
-for (var i = 0; i < collapseAboveLinks.length; i++) {
-    collapseAboveLinks[i].addEventListener('click', collapseAbove, false);
+var collapsers = document.getElementsByClassName('collapser');
+for (var i = 0; i < collapsers.length; i++) {
+    collapsers[i].addEventListener('click', collapse, false);
 }
